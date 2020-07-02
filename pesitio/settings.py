738bae4ed92abf301fc0de8fb5ea52cb597056c3
evallardy <evallardy@gmail.com>
2020-07-02 14:18:20 -0500

    commit ok mysql

--- pesitio/settings.py
+++ pesitio/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': '/pesitio/pesitio.cnf',
+            'read_default_file': 'pesitio/pesitio.cnf',
         },
     }
 }
